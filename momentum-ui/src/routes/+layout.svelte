e02a1279d99a24781957f3f61c592e8a0d49de1a
Jan Lauber <jan.lauber@protonmail.ch>
2023-05-22 15:48:26 +0200

    feat: add page transition
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- momentum-ui/src/routes/+layout.svelte
+++ momentum-ui/src/routes/+layout.svelte
@@ -5,13 +5,14 @@
   import { AppShell } from "@skeletonlabs/skeleton";
   import Nav from "$lib/components/base/Nav.svelte";
   import { page } from "$app/stores";
-  import { FolderGit2, LayoutDashboard, FileCode, Layers, Package } from "lucide-svelte";
+  import { FolderGit2, LayoutDashboard, FileCode, Layers, Package, Loader2 } from "lucide-svelte";
   import type { NavRoute } from "$lib/types";
-  import { onMount } from "svelte";
-  import { fly } from "svelte/transition";
   import { metadata } from "$lib/app/store";
   import { site } from "$lib/config";
   import { beforeNavigate } from "$app/navigation";
+  import { fly } from "svelte/transition";
+
+  export let data: any;
 
   $: title = $metadata.title ? $metadata.title + " | " + site.name : site.name;
   $: description = $metadata.description ?? site.description;
@@ -19,9 +20,6 @@
   beforeNavigate(() => {
     $metadata = {};
   });
-
-  let ready = false;
-  onMount(() => (ready = true));
 
   let routes: NavRoute[] = [
     {
@@ -83,13 +81,23 @@
       <Nav {routes} />
     </svelte:fragment>
   </AppShell>
-  <div
-    class="absolute pr-4 pt-4 pb-4 left-64 right-0 top-32 bottom-0 overflow-y-scroll hide-scrollbar"
-  >
-    {#if ready}
-      <div transition:fly={{ y: 100, duration: 200 }}>
-        <slot />
+  <!-- <div
+      class="absolute top-0 left-0 right-0 bottom-0 bg-white bg-opacity-0 flex justify-center items-center z-40 animate-pulse"
+    />
+    <div class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50">
+      <div class="flex flex-col items-center">
+        <Loader2 class="w-16 h-16 text-primary-500 animate-spin" />
+        <p class="text-gray-500 font-bold text-2xl ml-4">Loading...</p>
       </div>
-    {/if}
-  </div>
+    </div>
+  {:else} -->
+  {#key data.pathname}
+    <div
+      class="absolute pr-4 pt-4 pb-4 left-64 right-0 top-32 bottom-0 overflow-y-scroll hide-scrollbar"
+      in:fly={{ y: 50, duration: 100, delay: 100 }}
+      out:fly={{ y: 50, duration: 100 }}
+    >
+      <slot />
+    </div>
+  {/key}
 </div>
