3defd9d391be16869a65739191bf4eac8a68ae80
Jan Lauber <jan.lauber@protonmail.ch>
2023-05-23 09:52:30 +0200

    feat: enhance hovering
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- momentum-ui/src/lib/components/repositories/RepositoryCard.svelte
+++ momentum-ui/src/lib/components/repositories/RepositoryCard.svelte
@@ -12,15 +12,15 @@
 
 </script>
 
-<div class="card rounded-md border-2
+<div class="card rounded-md border-2 transition-all duration-300 ease-in-out hover:shadow-lg
   {
     repository.status === RepositoriesStatusOptions['UP-TO-DATE']
-      ? 'border-green-300'
+      ? 'border-green-400 hover:border-green-500'
       : repository.status === RepositoriesStatusOptions.ERROR
-      ? 'border-red-300'
+      ? 'border-red-400 hover:border-red-500'
       : repository.status === RepositoriesStatusOptions.PENDING
-      ? 'border-yellow-300'
-      : 'border-gray-300'
+      ? 'border-yellow-400 hover:border-yellow-500'
+      : 'border-gray-400 hover:border-gray-500'
   }
 ">
   <div class="flex">
@@ -28,61 +28,37 @@
       on:click={onHandleStatusClick}
       class="p-6 rounded-l-md cursor-pointer w-52 group hover:w-full transition-all duration-300 ease-in-out
       {repository.status === RepositoriesStatusOptions['UP-TO-DATE']
-        ? 'bg-green-300'
+        ? 'bg-green-400 hover:bg-green-500'
         : repository.status === RepositoriesStatusOptions.ERROR
-        ? 'bg-red-300'
+        ? 'bg-red-400 hover:bg-red-500'
         : repository.status === RepositoriesStatusOptions.PENDING
-        ? 'bg-yellow-300'
-        : 'bg-gray-300'}
+        ? 'bg-yellow-400 hover:bg-yellow-500'
+        : 'bg-gray-400 hover:bg-gray-500'}
     "
     >
 
         <div class="flex flex-col justify-center items-center ">
           {#if repository.status === RepositoriesStatusOptions.PENDING}
-          <RefreshCw />
+          <RefreshCw class="text-white group-hover:rotate-180 transition-all duration-300 ease-in-out" />
           {:else if repository.status === RepositoriesStatusOptions.ERROR}
-          <Ban />
+          <Ban class="text-white" />
           {:else if repository.status === RepositoriesStatusOptions['UP-TO-DATE']}
-          <Check />
+          <Check class="text-white" />
           {:else if repository.status === RepositoriesStatusOptions.SYNCING}
-          <RefreshCw class="animate-spin" />
+          <RefreshCw class="animate-spin text-white" />
           {/if}
-          <p class=" text-xs">
+          <p class=" text-xs text-white">
             {repository.status}
           </p>
         </div>
     </button>
-    <div class="flex flex-col justify-center items-center w-full relative">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 ">
+    <div class="flex flex-col justify-center items-center w-full relative ">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">
         {repository.name}
       </h3>
       <a href={"/repositories/" + repository.id} class="absolute right-2 top-2">
-        <Settings2 class="text-gray-900" />
+        <Settings2 class="text-gray-900 hover:rotate-180 transition-all duration-150 ease-in-out" />
       </a>
     </div>
   </div>
 </div>
-
-<!-- <div class="absolute left-0 right-0 w-full top-0">
-  {#if repository.status === RepositoriesStatusOptions["UP-TO-DATE"]}
-    <span class="badge variant-filled bg-green-500 w-full rounded-t-md">
-      <Check />{@html "&nbsp;"}
-      {repository.status}
-    </span>
-  {:else if repository.status === RepositoriesStatusOptions.ERROR}
-    <span class="badge variant-filled bg-red-500 w-full rounded-t-md">
-      <Ban />{@html "&nbsp;"}
-      {repository.status}
-    </span>
-  {:else if repository.status === RepositoriesStatusOptions.SYNCING}
-    <span class="badge variant-filled bg-yellow-500 w-full rounded-t-md">
-      <RefreshCw class="animate-spin" />{@html "&nbsp;"}
-      {repository.status}
-    </span>
-  {:else if repository.status === RepositoriesStatusOptions.PENDING}
-    <span class="badge variant-filled bg-gray-500 w-full rounded-l-md">
-      <RefreshCw />{@html "&nbsp;"}
-      {repository.status}
-    </span>
-  {/if}
-</div> -->
