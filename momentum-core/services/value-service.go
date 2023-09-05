cca3669c70f265896d5eb91be319951b9e7a01ea
Joel-Haeberli <haebu@rubigen.ch>
2023-09-05 22:12:55 +0200

    feat: read values of application

--- momentum-core/services/value-service.go
+++ momentum-core/services/value-service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"fmt"
 	"momentum-core/config"
 	"momentum-core/models"
 	"strings"
@@ -44,6 +45,7 @@ func (vs *ValueService) ValuesByApplication(repositoryName string, applicationId
 	wrappedValues := make([]*models.ValueWrapper, 0)
 	files := application.Files()
 	for _, f := range files {
+		fmt.Println("File:", f.NormalizedPath())
 		if strings.EqualFold(f.NormalizedPath(), KUSTOMIZATION_FILE_NAME) {
 			wrappedKustomization, err := models.ToValueWrapperFromNode(f, models.APPLICATION)
 			if err != nil {
