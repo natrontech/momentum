7c96fb5eb0ff9d4877077dff4c605812d0b565ea
Jan Lauber <jan.lauber@protonmail.ch>
2023-09-19 14:23:07 -0700

    feat: save
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- momentum-ui/src/lib/components/base/Nav.svelte
+++ momentum-ui/src/lib/components/base/Nav.svelte
@@ -6,48 +6,44 @@
   export let routes: NavRoute[] = [];
 </script>
 
-<div class="h-full p-4 bg-transparent">
-  <div class="bg-white shadow-md h-full rounded-lg w-56 z-20 relative">
-    <div class="flex flex-shrink-0 items-center pt-4 px-8 flex-col">
-      <img class="h-12 w-auto" src="/images/momentum-logo.png" alt="Momentum Logo" />
-      <p class="mt-2 text-xl font-bold text-primary-500 uppercase">Momentum</p>
-    </div>
+<div class="bg-black shadow-md h-full w-24 z-20 relative">
+  <div class="flex flex-shrink-0 items-center pt-12 px-6 flex-col">
+    <img class="h-12 w-full" src="/images/momentum-logo.png" alt="Momentum Logo" />
+  </div>
 
-    <!-- Navigation -->
-    <nav class="mt-3 px-3 pt-6">
-      <div class="space-y-4">
-        {#each routes as route}
-          <a
-            href={route.href}
-            class="text-gray-500 unstyled border-2 rounded-md group flex items-center hover:text-gray-600 pl-9 py-2 text-sm font-medium
+  <!-- Navigation -->
+  <nav class="mt-3 px-3 pt-6">
+    <div class="space-y-4">
+      {#each routes as route}
+        <a
+          href={route.href}
+          class="text-gray-500 unstyled border-2 rounded-md group flex items-center hover:text-gray-600 p-2 text-sm font-medium
 						{$page.route.id.split('/')[1]?.includes(route.name.toLowerCase()) ||
-            ($page.route.id === '/' && route.name === 'Dashboard')
-              ? 'text-gray-700 border-gray-700'
-              : ''}
+          ($page.route.id === '/' && route.name === 'Dashboard')
+            ? 'text-gray-700 border-gray-700'
+            : ''}
 "
-            aria-current="page"
-          >
-            {#if $page.route.id
-              .split("/")[1]
-              ?.includes(route.name.toLowerCase()) || ($page.route.id === "/" && route.name === "Dashboard")}
-              <!-- <Icon className="w-5 h-5" src={route.currentIcon} />{@html '&nbsp;'} -->
-              <svelte:component this={route.icon} class="w-5 h-5" />{@html "&nbsp;"}
-              <div class="block">{route.name}</div>
-            {:else}
-              <svelte:component this={route.icon} class="w-5 h-5" />{@html "&nbsp;"}{route.name}
-            {/if}
-          </a>
-        {/each}
-      </div>
-    </nav>
-    <div class="bottom-6 absolute w-full flex items-center justify-center">
-      <!-- <a
+          aria-current="page"
+        >
+          {#if $page.route.id
+            .split("/")[1]
+            ?.includes(route.name.toLowerCase()) || ($page.route.id === "/" && route.name === "Dashboard")}
+            <!-- <Icon className="w-5 h-5" src={route.currentIcon} />{@html '&nbsp;'} -->
+            <svelte:component this={route.icon} class="w-7 h-7" />{@html "&nbsp;"}
+          {:else}
+            <svelte:component this={route.icon} class="w-7 h-7" />
+          {/if}
+        </a>
+      {/each}
+    </div>
+  </nav>
+  <!-- <div class="bottom-6 absolute w-full flex items-center justify-center">
+    <a
         href="/logout"
         class="text-gray-500 unstyled group flex items-center hover:text-gray-600 px-2 py-2 text-sm font-medium"
         aria-current="page"
       >
         <Icon className="w-5 h-5" src={HiOutlineLogout} />{@html "&nbsp;"}Sign-out
-      </a> -->
-    </div>
-  </div>
+      </a>
+  </div> -->
 </div>
